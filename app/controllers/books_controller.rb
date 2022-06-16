7efd20ad8d0a7ec65ae29234c5431bd3e619c747
okazaki1224 <101001729+okazaki1224@users.noreply.github.com>
2022-06-16 17:07:23 +0000

    [Add]バリデーションとエラーメッセージを設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,10 +26,15 @@ class BooksController < ApplicationController
   end
   
   def create
+    
     @book=Book.new(book_params)
     @book.user_id=current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books=Book.all
+      render :index
+    end
   end
   
   def destroy
