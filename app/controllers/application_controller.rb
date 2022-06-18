ba9d05382d0ff650f5a004386d9e3676dd9f7443
okazaki1224 <101001729+okazaki1224@users.noreply.github.com>
2022-06-18 15:12:41 +0000

    [Update]サインイン直後のページをユーザー詳細に設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,7 @@ class ApplicationController < ActionController::Base
    
    #マイページ設定
    def after_sign_in_path_for(resource) 
-      books_path
-      
+      user_path(current_user.id)
    end
    
    protected
