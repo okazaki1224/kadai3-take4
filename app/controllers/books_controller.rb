0b23607490993bf73e0c9910a1a6510e304ffb62
okazaki1224 <101001729+okazaki1224@users.noreply.github.com>
2022-06-22 09:26:54 +0000

    [Update]エラー残り0個、完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action:ensure,only:[:edit, :update]
+
   def new
   end
 
@@ -18,38 +20,45 @@ class BooksController < ApplicationController
   def edit
     @book=Book.find(params[:id])
   end
-  
+
   def update
     @book=Book.find(params[:id])
     if @book.update(book_params)
      redirect_to book_path(@book.id),notice: "You have updated book successfully."
     else
       render :edit
-    end  
+    end
   end
-  
+
   def create
-    
+
     @book=Book.new(book_params)
     @book.user_id=current_user.id
     if @book.save
       redirect_to book_path(@book.id),notice: "You have creaated book successfully."
-      
+
     else
       @books=Book.all
       render :index
     end
   end
-  
+
   def destroy
     book=Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
+   def ensure
+    @book=Book.find(params[:id])
+    unless @book.user==current_user
+    redirect_to books_path
+    end
+   end
+
 end
