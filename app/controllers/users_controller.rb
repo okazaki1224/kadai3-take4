bcb232fb5d95cfa18e0c8779f6f7d23d8fcaecb2
okazaki1224 <101001729+okazaki1224@users.noreply.github.com>
2022-06-22 08:56:05 +0000

    [Update]エラー残り1個()

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action:ensure,only:[:edit, :update]
   def index
     @users=User.all
     @new_book=Book.new
@@ -13,29 +14,36 @@ class UsersController < ApplicationController
   def edit
       @user=User.find(params[:id])
   end
-  
+
   def create
   　@book=Book.new(book_params)
     @book.user_id=current_user.id
     @book.save
     redirect_to book_path(@book.id),notice: "You have creaated book successfully."
   end
-  
+
   def update
       @user=User.find(params[:id])
       if @user.update(user_params)
         redirect_to user_path(@user.id),notice: "You have updated profile successfully."
       else
         render :edit
-      end  
+      end
   end
 
   private
   def user_params
       params.require(:user) .permit(:name, :introduction, :profile_image)
   end
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def ensure
+    @user=User.find(params[:id])
+   unless @user==current_user
+    redirect_to user_path(current_user)
+   end
   end
 end
